f8ec81b4d6a029f0a38c0c59031f79d7654d19d5
DreadnaughtSec <brianrpeters@gmail.com>
2017-07-19 05:04:35 -0500

    added mode support, info in readme

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # GeoPy
 Python based IP geolocation
-Uses the pygeoip library to id info about an IP address.
-Current config is to kick out the state and lat/lon data.
+Uses the pygeoip library and GeoLiteCity database to id info about IP addresses
+Currently returns IP, Country, City, Region, and lat/long data
 
- 
+usage: 
+
+	single target
+	geopy.py -n <ipaddress>
+
+	list mode
+	geopy.py -l </nameoffile>    
+
+Some countries will thrown exceptions for city and region, so they will respond with "none". Cayman Islands would be an example where city and region are "none". 
--- geopy.py
+++ geopy.py
@@ -4,12 +4,28 @@ from pygeoip import GeoIP
 #load IP location database to variable
 geo = GeoIP('GeoLiteCity.dat')
 
-#grab IP address from cli arg
-ip = sys.argv[1]
+#get mode arg
+mode = sys.argv[1]
 
-ipInfo = geo.record_by_addr(ip)
-print "Target: " + ip
-print "Country: " + ipInfo['country_name']
-print "Region: " + ipInfo['city'] + ", " + ipInfo['region_code']
-print "GPS: " + str(ipInfo['latitude']),",",str(ipInfo['longitude'])
+#get ip for normal mode
+ip = sys.argv[2]
 
+if(mode == "-n"):
+	ipInfo = geo.record_by_addr(ip)
+	print "Target: " + ip
+	print "Country: " + str(ipInfo['country_name'])
+	print "Region: " + str(ipInfo['city']) + ", " + str(ipInfo['region_code'])
+	print "GPS: " + str(ipInfo['latitude']),",",str(ipInfo['longitude'])
+elif(mode == "-l"):
+	ips = sys.argv[2]
+	ipList = open(ips)
+	
+	for IP in ipList.readlines():
+		ipInfo = geo.record_by_addr(IP)
+		print "Target: " + str(IP).strip()
+		print "Country: " + str(ipInfo['country_name'])
+		print "Region: " + str(ipInfo['city']) + ", " + str(ipInfo['region_code'])
+		print "GPS: " + str(ipInfo['latitude']) + ", " + str(ipInfo['longitude'])
+		print ""
+else:
+	print "Something went wrong"
