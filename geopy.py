7d34d24914cd8def2bae11dfc141432e31123a68
DreadnaughtSec <brianrpeters@gmail.com>
2017-07-19 04:22:41 -0500

    IP is now taken as cli arg

--- geopy.py
+++ geopy.py
@@ -1,8 +1,15 @@
+import sys
 from pygeoip import GeoIP
 
+#load IP location database to variable
 geo = GeoIP('GeoLiteCity.dat')
 
-ipInfo = geo.record_by_addr('72.14.199.81')
-print ipInfo['country_name']
-print ipInfo['latitude'],",",ipInfo['longitude']
+#grab IP address from cli arg
+ip = sys.argv[1]
+
+ipInfo = geo.record_by_addr(ip)
+print "Target: " + ip
+print "Country: " + ipInfo['country_name']
+print "Region: " + ipInfo['city'] + ", " + ipInfo['region_code']
+print "GPS: " + str(ipInfo['latitude']),",",str(ipInfo['longitude'])
 
