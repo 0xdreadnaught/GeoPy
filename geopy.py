97afe3339405384b15f066da933de905919cdf1b
DreadnaughtSec <brianrpeters@gmail.com>
2017-07-19 08:53:35 -0500

    added -p <file> for processing pcap dumps

--- geopy.py
+++ geopy.py
@@ -1,4 +1,6 @@
 import sys
+import dpkt
+import socket
 from pygeoip import GeoIP
 
 #load IP location database to variable
@@ -27,5 +29,43 @@ elif(mode == "-l"):
 		print "Region: " + str(ipInfo['city']) + ", " + str(ipInfo['region_code'])
 		print "GPS: " + str(ipInfo['latitude']) + ", " + str(ipInfo['longitude'])
 		print ""
+elif(mode == "-p"):
+	pcap = sys.argv[2]
+	pcapdump = open(pcap, 'rb')
+	pcapReader = dpkt.pcap.Reader(pcapdump)
+
+	iplist = []
+	
+	for time,frame in pcapReader:
+		eth = dpkt.ethernet.Ethernet(frame)
+		if(eth.type != dpkt.ethernet.ETH_TYPE_IP): continue
+		ip = eth.data		
+		src = socket.inet_ntoa(ip.src)
+		dst = socket.inet_ntoa(ip.dst)
+		if not(src in iplist):
+			iplist.append(src)
+		if not(dst in iplist):
+			iplist.append(dst)
+
+	for IP in iplist:				
+		ipInfo = geo.record_by_addr(IP)
+		
+		#now we need to check the first octet of the address to skip over local IPs
+		#first we'll handle 
+		if(IP.startswith("10."))  or (IP.startswith("192.")) or (IP.startswith("172.")) or (IP.startswith("127.")):
+	                #do nothing
+			pass
+		elif("." not in IP[:3]):
+			if(IP > 223):
+				pass
+		elif(IP == "0.0.0.0"):
+			pass
+		else:
+			print "Target: " + str(IP).strip()
+			print "Country: " + str(ipInfo['country_name'])
+			print "Region: " + str(ipInfo['city']) + ", " + str(ipInfo['region_code'])
+			print "GPS: " + str(ipInfo['latitude']) + ", " + str(ipInfo['longitude'])
+	                print ""
+			
 else:
 	print "Something went wrong"
