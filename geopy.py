cc829f6fe752e160dbae5ed1bd5f502f86102667
DreadnaughtSec <brianrpeters@gmail.com>
2017-07-20 01:30:16 -0500

    added whois org info and bug fixes

--- geopy.py
+++ geopy.py
@@ -1,8 +1,30 @@
 import sys
-import dpkt
-import socket
 import re
+import urllib2
+import html2text
+import socket
 from pygeoip import GeoIP
+
+def getISP(ip):
+	tracker = "http://www.ip-adress.com/ip_tracer/"
+	header = {'User-Agent': 'Mozilla/5.0'}
+	request = urllib2.Request(tracker + ip, headers=header)
+	data = urllib2.urlopen(request).read()
+	html = html2text.HTML2Text()
+	html.ignore_links = True
+	info = html.handle(data)
+	cleanInfo = info.encode('ascii', 'ignore').decode('ascii')
+	
+	lines = cleanInfo.splitlines()
+	for line in lines:
+		if ("Organization| " in line):
+			org = line
+
+
+	if(len(org) > 0):
+		print "\033[1;32;40m Organization:\033[0;37;40m " + org[14:] 
+	else:
+		print "\033[1;32;40m Organization:\033[0;37;40m Uknown"
 
 def skipIP(ip):
 	print "\033[0;31;40m Skipping: " + "\033[0;37;40m" + ip.strip() + "\n"
@@ -11,7 +33,9 @@ def printInfo(ip, info):
 	print "\033[1;32;40m Target: " + "\033[0;37;40m" + str(ip).strip()
         print "\033[1;32;40m Country: " + "\033[0;37;40m" + str(info['country_name'])
         print "\033[1;32;40m Region: " + "\033[0;37;40m" + str(info['city']) + ", " + str(info['region_code'])
-        print "\033[1;32;40m GPS: " + "\033[0;37;40m" + str(info['latitude']) + ", " + str(info['longitude']) + "\n"
+        print "\033[1;32;40m GPS: " + "\033[0;37;40m" + str(info['latitude']) + ", " + str(info['longitude'])
+	
+	getISP(ip)
 
 def main():
 	#load IP location database to variable
@@ -30,7 +54,7 @@ def main():
 		if(ip.startswith("10."))  or (ip.startswith("192.")) or (ip.startswith("172.")) or (ip.startswith("127.") or (ip.startswith("0."))):
 			skipIP(ip)
 		elif("." not in ip[:3]):
-			if(IP > 223):
+			if(int(ip[:3]) > 223):
 				skipIP(ip)
 		else:
 			printInfo(ip, ipInfo)
@@ -67,7 +91,7 @@ def main():
 				iplist.append(src)
 			if not(dst in iplist):
 				iplist.append(dst)
-	
+
 		for IP in iplist:				
 			ipInfo = geo.record_by_addr(IP)
 			#now we need to check the first octet of the address to skip over local IPs
@@ -76,7 +100,7 @@ def main():
 				skipIP(IP)
 			elif("." not in IP[:3]):
 				if(IP > 223):
-					skipIP(IP)
+					pass
 			else:
                         	printInfo(IP, ipInfo)			
 	else:
