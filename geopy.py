4442b574943ba40b4a61efd16e276a9b737c4db2
DreadnaughtSec <brianrpeters@gmail.com>
2017-07-19 21:20:10 -0500

    removed test copy

--- geopy.py
+++ geopy.py
@@ -4,100 +4,82 @@ import socket
 import re
 from pygeoip import GeoIP
 
-#load IP location database to variable
-geo = GeoIP('GeoLiteCity.dat')
+def skipIP(ip):
+	print "\033[0;31;40m Skipping: " + "\033[0;37;40m" + ip.strip() + "\n"
 
-#get mode arg
-mode = sys.argv[1]
+def printInfo(ip, info):
+	print "\033[1;32;40m Target: " + "\033[0;37;40m" + str(ip).strip()
+        print "\033[1;32;40m Country: " + "\033[0;37;40m" + str(info['country_name'])
+        print "\033[1;32;40m Region: " + "\033[0;37;40m" + str(info['city']) + ", " + str(info['region_code'])
+        print "\033[1;32;40m GPS: " + "\033[0;37;40m" + str(info['latitude']) + ", " + str(info['longitude']) + "\n"
 
-#get ip for normal mode
-ip = sys.argv[2]
-
-if(mode == "-n"):
-	ipInfo = geo.record_by_addr(ip)
-        #now we need to check the first octet of the address to skip over local IPs
-        #first we'll handle
-        if(ip.startswith("10."))  or (ip.startswith("192.")) or (ip.startswith("172.")) or (ip.startswith("127.") or (ip.startswith("0."))):
-                print "################"
-		print "Skipping " + ip.strip()
-		print "################"
-		print ""
-        elif("." not in ip[:3]):
-                if(IP > 223):
-                        print "################"
-      	                print "Skipping " + ip.strip()
-                        print "################"
-			print ""
-                else:
-                        print "Target: " + str(ip).strip()
-                        print "Country: " + str(ipInfo['country_name'])
-                        print "Region: " + str(ipInfo['city']) + ", " + str(ipInfo['region_code'])
-                        print "GPS: " + str(ipInfo['latitude']) + ", " + str(ipInfo['longitude'])
-                        print ""
-
-elif(mode == "-l"):
-	ips = sys.argv[2]
-	ipList = open(ips)
+def main():
+	#load IP location database to variable
+	geo = GeoIP('GeoLiteCity.dat')
 	
-	for IP in ipList.readlines():
-		ipInfo = geo.record_by_addr(IP)
+	#get mode arg
+	mode = sys.argv[1]
+	
+	#get ip for normal mode
+	ip = sys.argv[2]
+
+	if(mode == "-n"):
+		ipInfo = geo.record_by_addr(ip)
 		#now we need to check the first octet of the address to skip over local IPs
-                #first we'll handle
-                if(IP.startswith("10."))  or (IP.startswith("192.")) or (IP.startswith("172.")) or (IP.startswith("127.") or (IP.startswith("0."))):
-                        print "################"
-                        print "Skipping " + IP.strip()
-                        print "################"
-			print ""
-                elif("." not in IP[:3]):
-                        if(IP > 223):
-                                print "################"
-        	                print "Skipping " + IP.strip()
-	                        print "################"
-				print ""
-                else:
-                        print "Target: " + str(IP).strip()
-                        print "Country: " + str(ipInfo['country_name'])
-                        print "Region: " + str(ipInfo['city']) + ", " + str(ipInfo['region_code'])
-                        print "GPS: " + str(ipInfo['latitude']) + ", " + str(ipInfo['longitude'])
-                        print ""
-elif(mode == "-p"):
-	pcap = sys.argv[2]
-	pcapdump = open(pcap, 'rb')
-	pcapReader = dpkt.pcap.Reader(pcapdump)
-
-	iplist = []
+		#first we'll handle
+		if(ip.startswith("10."))  or (ip.startswith("192.")) or (ip.startswith("172.")) or (ip.startswith("127.") or (ip.startswith("0."))):
+			skipIP(ip)
+		elif("." not in ip[:3]):
+			if(IP > 223):
+				skipIP(ip)
+		else:
+			printInfo(ip, ipInfo)
 	
-	for time,frame in pcapReader:
-		eth = dpkt.ethernet.Ethernet(frame)
-		if(eth.type != dpkt.ethernet.ETH_TYPE_IP): continue
-		ip = eth.data		
-		src = socket.inet_ntoa(ip.src)
-		dst = socket.inet_ntoa(ip.dst)
-		if not(src in iplist):
-			iplist.append(src)
-		if not(dst in iplist):
-			iplist.append(dst)
-
-	for IP in iplist:				
-		ipInfo = geo.record_by_addr(IP)
-                #now we need to check the first octet of the address to skip over local IPs
-                #first we'll handle
-                if(IP.startswith("10."))  or (IP.startswith("192.")) or (IP.startswith("172.")) or (IP.startswith("127.") or (IP.startswith("0."))):
-                        print "################"
-                        print "Skipping " + IP.strip()
-                        print "################"
-			print ""
-                elif("." not in IP[:3]):
-                        if(IP > 223):
-                                print "################"
-        	                print "Skipping " + IP.strip()
-	                        print "################"
-				print ""
-                else:
-                        print "Target: " + str(IP).strip()
-                        print "Country: " + str(ipInfo['country_name'])
-                        print "Region: " + str(ipInfo['city']) + ", " + str(ipInfo['region_code'])
-                        print "GPS: " + str(ipInfo['latitude']) + ", " + str(ipInfo['longitude'])
-                        print ""			
-else:
-	print "Something went wrong"
+	elif(mode == "-l"):
+		ips = sys.argv[2]
+		ipList = open(ips)
+		
+		for IP in ipList.readlines():
+			ipInfo = geo.record_by_addr(IP)
+			#now we need to check the first octet of the address to skip over local IPs
+			#first we'll handle
+			if(IP.startswith("10."))  or (IP.startswith("192.")) or (IP.startswith("172.")) or (IP.startswith("127.") or (IP.startswith("0."))):
+				skipIP(IP)
+			elif("." not in IP[:3]):
+				if(IP > 223):
+					skipIP(IP)
+			else:
+				printInfo(IP, ipInfo)
+	elif(mode == "-p"):
+		pcap = sys.argv[2]
+		pcapdump = open(pcap, 'rb')
+		pcapReader = dpkt.pcap.Reader(pcapdump)
+	
+		iplist = []
+		
+		for time,frame in pcapReader:
+			eth = dpkt.ethernet.Ethernet(frame)
+			if(eth.type != dpkt.ethernet.ETH_TYPE_IP): continue
+			ip = eth.data		
+			src = socket.inet_ntoa(ip.src)
+			dst = socket.inet_ntoa(ip.dst)
+			if not(src in iplist):
+				iplist.append(src)
+			if not(dst in iplist):
+				iplist.append(dst)
+	
+		for IP in iplist:				
+			ipInfo = geo.record_by_addr(IP)
+			#now we need to check the first octet of the address to skip over local IPs
+			#first we'll handle
+			if(IP.startswith("10."))  or (IP.startswith("192.")) or (IP.startswith("172.")) or (IP.startswith("127.") or (IP.startswith("0."))):
+				skipIP(IP)
+			elif("." not in IP[:3]):
+				if(IP > 223):
+					skipIP(IP)
+			else:
+                        	printInfo(IP, ipInfo)			
+	else:
+		print "Something went wrong"
+		
+main()
